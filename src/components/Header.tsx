77ad90b52f6da79d8df72d5ce40458efa26b923d
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-07-13 11:29:13 -0300

    feat(Header): Iniciando o projeto TodoList e criando o componente header com a importação da logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Logo from "../../src/assets/Logo.svg";
+import styles from '../styles/Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="Logotipo" />
+    </header>
+    
+    )
+    
+}
+
+export { Header };

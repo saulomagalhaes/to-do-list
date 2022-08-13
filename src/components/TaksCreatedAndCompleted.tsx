2172d961f8b7b790e30b04f5189f48aa61947e59
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-08-13 16:17:53 -0300

    feat(TasksCreatedAndCompleted): :sparkles: implementando contador de tarefas criadas e concluídas

--- src/components/TaksCreatedAndCompleted.tsx
+++ src/components/TaksCreatedAndCompleted.tsx
@@ -1,14 +1,20 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 import styles from "../styles/TaksCreatedAndCompleted.module.scss";
 
 function TaksCreatedAndCompleted() {
+  const tasks = useSelector((state: RootState) => state);
+  const totalTasks = tasks.length;
+  const completed = tasks.filter((t) => t.isCompleted).length;
+  
   return (
     <div className={styles.container}>
       <div className={styles.TaksCreatedAndCompleted}>
         <p>
-          Tarefas Criadas <span>0</span>
+          Tarefas Criadas <span>{totalTasks}</span>
         </p>
         <p>
-          Concluídas <span>0</span>
+          Concluídas <span>{`${completed} de ${totalTasks}`}</span>
         </p>
       </div>
     </div>
